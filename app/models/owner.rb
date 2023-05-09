2036baa20420cac7230b879c99a566c3d7fe79c7
jesus <jeadroar05@gmail.com>
2023-05-08 20:39:35 -0600

    LAB2 C16536 y C18512 WIP

---
+++ app/models/owner.rb
@@ -0,0 +1,11 @@
+class Owner < ApplicationRecord
+
+    validates :first_name, presence: true, length: { minimum: 2, maximum: 10 }
+    validates :last_name, presence: true, length: { minimum: 2, maximum: 15 }
+    validates :email, presence: true, format: { with: /\A[\w+-.]+@[a-z\d-]+(.[a-z]+)*.[a-z]+\z/i,
+                                                message: "Correo inválido" }
+    validates :number_id, presence: true, uniqueness: true, length: { minimum: 5, maximum: 8}
+    validates :level, presence: true
+
+    enum level: {'bronze': 0, 'silver': 1, 'gold': 2}
+end
---
+++ spec/models/owner_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+RSpec.describe Owner, type: :model do
+  subject { 
+    described_class.new(first_name: "Jesus",
+                        last_name: "Rodriguez",
+                        email: "smpp@gmail.com",
+                        number_id: 2846338,
+                        level: 2)
+  }
+
+  it "valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without first_name" do
+    subject.first_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without last_name" do
+    subject.last_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without number_id" do
+    subject.number_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without level" do
+    subject.level = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a first_name with less than 2 letters" do
+    subject.first_name = "a"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a last_name with less than 2 letters" do
+    subject.last_name = "a"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a first_name with more than 10 letters" do
+    subject.first_name = "abcdefghijklmnopqrstu"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a last_name with more than 15 letters" do
+    subject.last_name = "abcdefghijklmnopqrstu"
+    expect(subject).to_not be_valid
+  end
+
+  # it "is not valid with a number_id with less than 5 letters" do
+  #   subject.number_id = "a"
+  #   expect(subject).to_not be_valid
+  # end
+
+  # it "is not valid with a number_id with more than 8 letters" do
+  #   subject.number_id = "abcdefghijklmnopqrstu"
+  #   expect(subject).to_not be_valid
+  # end
+end
