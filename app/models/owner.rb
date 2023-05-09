38fefc6ed0a3635642885499616d827b1b8a3880
BCHDavid <hidalgodavid850@gmail.com>
2023-05-09 17:50:55 -0600

    LAB2-B73808_C18377 update

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -3,7 +3,8 @@ class Owner < ApplicationRecord
 
     validates :first_name, presence: true, length: { maximum: 10 }
     validates :last_name, presence: true, length: { maximum: 15 }
-    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }   
+    #validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }  
     validates :owner_id, presence: true, length: { minimum: 5, maximum: 8 }, uniqueness: true
     validates :level, presence: true
 
