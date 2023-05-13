657ae72aae34355c200fb4d18d2a5da0d1ac67fe
Baigv25 <baigv25@gmail.com>
2023-05-13 11:54:16 -0600

    BGV-C03471 Lab correction pet.rb inclusion

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,6 +2,6 @@ class Pet < ApplicationRecord
     belongs_to :owner
     validates :name, presence: true, length: { maximum: 10 }
     validates :breed, presence: true, length: { maximum: 10 }
-    validates :pedigree, presence: true
+    validates :pedigree, presence: true, inclusion: [true, false]
 
 end
