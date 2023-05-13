2356173a7838143d930474088c4c21db4aa6eca8
tyrsergio <sergio.hernandezjimenez@ucr.ac.cr>
2023-05-13 15:06:51 -0600

    SHJ-C13656 Lab2 done with changes

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,5 +3,5 @@ class Pet < ApplicationRecord
     belongs_to :owner
     validates :name, presence: true, length: {minimum: 3, maximum: 10}
     validates :breed, presence: true, length: {minimum: 3, maximum: 10}
-    validates :pedigree, presence: true, inclusion: { in: [true, false] }
+    validates :pedigree, inclusion: { in: [true, false] }
 end
